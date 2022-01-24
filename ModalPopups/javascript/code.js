752d189dcd7697618bcc9c3123f7fa7620f74887
Svendolin <sven0815@gmx.ch>
2022-01-25 00:14:54 +0100

    Added more files froim the lessons

--- ModalPopups/javascript/code.js
+++ ModalPopups/javascript/code.js
@@ -1,12 +1,12 @@
 
-// --- Open the whole popup via click by the popup-btn --- ///
+// --- OPEN the whole popup via click by the popup-btn --- ///
 document.getElementById("open-popup-btn").addEventListener("click", function(){
   document.getElementsByClassName("popup")
   [0].classList.add("active"); // This will add the active class as a first condition = 0 to the List
 });
 
 
-// --- Close the whole popup via click by the popup-btn --- ///
+// --- CLOSE the whole popup via click by the popup-btn --- ///
 document.getElementById("dismiss-popup-btn").addEventListener("click", function(){
   document.getElementsByClassName("popup")
   [0].classList.remove("active"); // This will remove the active class in return
